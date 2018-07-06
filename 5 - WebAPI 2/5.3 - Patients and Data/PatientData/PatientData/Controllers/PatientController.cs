8e5e06a1f02ec2d27bd61640c8d4099ee807cd43
deanwebbdeveloper <dean@deanwebbdeveloper.com>
2018-07-07 00:01:48 +0100

    Chapters 4-5
    Chapter 4 Completed
    
    Chapter 5 Started - Lectures 1-7 Completed

---
+++ 5 - WebAPI 2/5.3 - Patients and Data/PatientData/PatientData/Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PatientData.Controllers
+{
+    public class PatientController : ApiController
+    {
+        MongoCollection<Patient> _patients;
+
+        public PatientController()
+        {
+            _patients = PatientDb.Open();
+        }
+
+        public IEnumerable<Patient> Get()
+        {
+            return _patients.FindAll();
+        }
+    }
+}
---
+++ 5 - WebAPI 2/5.5 - Attribute Routing/PatientData/PatientData/Controllers/PatientController.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PatientData.Controllers
+{
+    public class PatientController : ApiController
+    {
+        MongoCollection<Patient> _patients;
+
+        public PatientController()
+        {
+            _patients = PatientDb.Open();
+        }
+
+        public IEnumerable<Patient> Get()
+        {
+            return _patients.FindAll();
+        }
+
+        public HttpResponseMessage Get(string id)
+        {
+            var patient = _patients.FindOneById(ObjectId.Parse(id));
+            if (patient == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Patient not found");
+            }
+            return Request.CreateResponse(patient);
+        }
+
+        [Route("api/patient/{id}/medications")]
+        public HttpResponseMessage GetMedications(string id)
+        {
+            var patient = _patients.FindOneById(ObjectId.Parse(id));
+            if (patient == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Patient not found");
+            }
+            return Request.CreateResponse(patient.Medications);
+        }
+    }
+}
