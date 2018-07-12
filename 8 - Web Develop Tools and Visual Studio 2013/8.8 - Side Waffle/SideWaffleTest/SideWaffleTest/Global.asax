97e76dcc73d0ca085d504a9d303fbca36db4902f
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-07-12 09:40:45 +0100

    Chapter 8 - Finishing
    Completed course

---
+++ 8 - Web Develop Tools and Visual Studio 2013/8.8 - Side Waffle/SideWaffleTest/SideWaffleTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SideWaffleTest.MvcApplication" Language="C#" %>
---
+++ 8 - Web Develop Tools and Visual Studio 2013/8.8 - Side Waffle/SideWaffleTest/SideWaffleTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SideWaffleTest.Startup))]
+namespace SideWaffleTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
